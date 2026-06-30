68c2266491a81b5234db80c14e39e59b703b8eb4
lolgamertopre <kasherwinbanquil@gmail.com>
2026-06-30 15:32:39 +0800

    Macto

--- StudentProfile.java
+++ StudentProfile.java
@@ -1,19 +1,15 @@
 
  
   public class StudentProfile {
-     String firstName;
-     String lastName;
-      int age;
- 
-     public StudentProfile(String firstName, String lastName, int age) {
-         this.firstName = firstName;
-         this.lastName = lastName;
-         this.age = age;
-     }
+
  
      public static void main(String[] args) {
-         StudentProfile student = new StudentProfile("John", "Doe", 20);
-        System.out.println("My name is " + student.firstName + " " + student.lastName + ". I am " + student.age + " years old");
+        String firstName = "John";
+        String lastName = "Doe";
+        int age = 20;
+        System.out.println("My name is " + firstName + " " + lastName + ". I am " + age + " years old");
      }
+     
+
  }
   
--- StudentProfileinput.java
+++ StudentProfileinput.java
@@ -1,14 +1,15 @@
-public class StudentProfileInput {
-    String firstname;
-    String lastname;
-    int age;
-    
-}
-static void main(String[] args) {
-    StudentProfileInput student = new StudentProfileInput();
-    student.firstname = "Alice";
-    student.lastname = "Smith";
-    student.age = 22;
-    
-    System.out.println("My name is " + student.firstname + " " + student.lastname + ". I am " + student.age + " years old");
+public class StudentProfileinput {
+       
+    static void main(String[] args) {
+        String firstname;
+        String lastname;
+        int age;
+
+        StudentProfileinput student = new StudentProfileinput();
+        student.firstname = "Alice";
+        student.lastname = "Smith";
+        student.age = 22;
+        
+        System.out.println("My name is " + student.firstname + " " + student.lastname + ". I am " + student.age + " years old");
+    }
 }
