130b3eaf526a39ab42adaa0b9f09beaf50d9edeb
lolgamertopre <kasherwinbanquil@gmail.com>
2026-07-01 14:25:23 +0800

    mama

--- CheckNumber.java
+++ CheckNumber.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
+// Determines the sign (positive, negative, zero) and parity (even, odd) of a number
 public class CheckNumber {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check the sign of the number
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        // Check the parity of the number
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+        scanner.close();
+    }
 }
